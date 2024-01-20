c8c625fe1bcff2d5ed3cfc54bbf689a47c756260
Ashley <139553561+AshTheDergy@users.noreply.github.com>
2024-01-20 18:09:37 +0200

    oops :<

--- settings/config.js
+++ settings/config.js
@@ -1,5 +1,5 @@
 require('dotenv').config({ path: './settings/.env' });
 module.exports = {
     TOKEN: process.env.TOKEN,
-    whiteList: ['909874278273523712'], //roles
+    whiteList: [], //roles
 }
